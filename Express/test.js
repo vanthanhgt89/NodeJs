3214018a74c60221330c15d0446c34c38cbbd268
vanthanh <vanthanh@techmasters-iMac-4.local>
2017-07-24 17:42:47 +0700

    up

--- Express/test.js
+++ Express/test.js
@@ -1,10 +1,34 @@
-let express = require('express')
+const express = require('express')
+const path = require('path')
 
-let app = express()
+
+
+const app = express()
+
+// ------ Body parse ----
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+app.use(bodyParser.json())
+
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// })
 
 app.get('/', (req, res) => {
-    // res.sendfile(__dirname + '/index.html')
-    res.render('index')
+    res.sendfile(__dirname + '/index.html')
+    // res.render('index')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.sendfile(__dirname + '/index.html')
+    // res.render('index')
 })
 app.get('/contact', (req, res) => {
     res.sendfile(__dirname + '/contact.html')
@@ -13,6 +37,11 @@ app.get('/contact', (req, res) => {
 app.get('/contact/:id', (req, res) => {
     res.send(' You request to see contact with id of ' + req.params.id)
 })
-app.listen(8000)
+app.listen(3000, err =>{
+    console.log('listen 3000');
+})
 
-console.log('Listen 8000');
+//req.query.name --> dung cho get: ? & ex: ?name
+//req.body.name ---> dung cho post
+
+//req.params.id ---> dung cho get dang : ex :id
